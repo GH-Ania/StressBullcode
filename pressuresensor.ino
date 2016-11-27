1f02701c63c1c10194b4746cb7c129e89cdacfbc
aniaelizabethm <aniamedrek@Anias-MacBook-Pro.local>
2016-11-27 01:17:12 -0500

    Stress bull toy

---
+++ pressuresensor.ino
@@ -0,0 +1,93 @@
+
+
+
+
+#include "config.h"
+
+
+/* FSR simple testing sketch. 
+ 
+Connect one end of FSR to power, the other end to Analog 0.
+Then connect one end of a 10K resistor from Analog 0 to ground 
+ 
+For more information see www.ladyada.net/learn/sensors/fsr.html */
+
+int sendRate = 1000;
+long lastSend;
+int fsrReading;     // the analog reading from the FSR resistor divider
+
+int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
+ 
+ 
+//int sensorVal;          //variable to hold the value from the sensor
+int prevSensorVal;
+int sensorChangeThreshold = 2;
+
+// set up the 'distVal' feed
+AdafruitIO_Feed *testfeed = io.feed("fsrReading");
+
+
+
+
+
+void setup(void) {
+  // We'll send debugging information via the Serial monitor
+  Serial.begin(115200);   
+
+  while(! Serial);
+
+  Serial.print("Connecting to Adafruit IO");
+
+  // connect to io.adafruit.com
+  io.connect();
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+ 
+void loop(void) {
+
+  fsrReading = analogRead(fsrPin);  
+ 
+  Serial.print("Analog reading = ");
+  Serial.print(fsrReading);     // the raw analog reading
+ 
+  // We'll have a few threshholds, qualitatively determined
+  if (fsrReading < 10) {
+    Serial.println(" - No pressure");
+  } else if (fsrReading < 200) {
+    Serial.println(" - Light touch");
+  } else if (fsrReading < 500) {
+    Serial.println(" - Light squeeze");
+  } else if (fsrReading < 800) {
+    Serial.println(" - Medium squeeze");
+  } else {
+    Serial.println(" - Big squeeze");
+  }
+  delay(1000);
+    io.run();
+  
+  //getValue from Sensor
+
+  if(abs((prevSensorVal-fsrReading))>sensorChangeThreshold) //check if there has been enough of a change in the value
+  {
+  // save randVal to the 'myRandoVal' feed on Adafruit IO
+  testfeed->save(fsrReading);
+  
+  //print the value to the serial monitor so you can check if its working
+  Serial.print("sending -> ");
+  Serial.println(fsrReading);
+  
+  }
+  prevSensorVal=fsrReading;
+
+  
+} 
